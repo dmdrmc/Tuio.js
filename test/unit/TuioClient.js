aa7a17de0e3b7e138ed93d4650438d728bdd6b99
Goran Košutić <kosutic.goran@gmail.com>
2015-11-17 13:21:45 +0100

    add test to see if trigger/on event handling works

--- test/unit/TuioClient.js
+++ test/unit/TuioClient.js
@@ -1,9 +1,14 @@
+(function() {
+
 var Tuio = require("../../src/Tuio");
 Tuio.Client = require("../../src/TuioClient");
+var client;
 
 QUnit.module("Tuio.Client", {
     setup: function() {
-
+        client = new Tuio.Client({
+            host: "http://localhost:5000"
+        });
     },
 
     teardown: function() {
@@ -11,10 +16,18 @@ QUnit.module("Tuio.Client", {
     }
 });
 
-test("construct", function() {
-    var client = new Tuio.Client({
-        host: "http://localhost:5000"
-    });
+QUnit.test("construct", function() {
 
     QUnit.equal(client.host, "http://localhost:5000");
 });
+
+QUnit.test("triggers refresh", function() {
+    
+    client.on("refresh", function(data) {
+        QUnit.equal(data, 1, "event data is not equal");
+    });
+    
+    client.trigger("refresh", 1);
+});
+
+})();
