f74496a04cc2002bb40fb1f82914d4f7800190ba
Goran Košutić <kosutic.goran@gmail.com>
2015-11-18 15:45:24 +0100

    clean up tests so it's easier to write osc messages

--- test/unit/TuioClient.js
+++ test/unit/TuioClient.js
@@ -4,6 +4,60 @@ var Tuio = require("../../src/Tuio");
 Tuio.Client = require("../../src/TuioClient");
 var client,
     server;
+    
+function writeOscMessage(address, args) {
+    
+    var arrayBuffer = new ArrayBuffer(1000),
+        bufferView = new DataView(arrayBuffer),
+        index = 0;
+    
+    function writeString(characters) {
+        var ui8View = new Uint8Array(arrayBuffer);
+        
+        for (var i = 0; i < characters.length; i+=1) {
+            ui8View[index] = characters[i].charCodeAt();
+            index += 1;
+        }
+        //null delimiter
+        ui8View[index] = 0;
+        index += 1;
+        // Round to the nearest 4-byte block. //osc.js
+        index = (index + 3) & ~0x03;
+    }
+    
+    // write address
+    writeString(address);
+    
+    if (args.length !== 0) {
+        
+        var typeTags = args.map(function(arg){
+            return arg.type;
+        });
+        typeTags.unshift(",");
+        writeString(typeTags.join(""));
+        
+        for( var i = 0; i < args.length; i += 1) {
+            var type = args[i].type,
+                value = args[i].value;
+            
+            switch(type) {
+                case "s":
+                    writeString(value);
+                    break;
+                case "i":
+                    bufferView.setUint32(index, value);
+                    index += 4;
+                    break;
+                case "f":
+                    bufferView.setFloat32(index, value);
+                    index += 4;
+                    break;
+            }
+        }
+    }
+    
+    return arrayBuffer;
+}
 
 QUnit.module("Tuio.Client", {
     setup: function() {
@@ -33,71 +87,75 @@ QUnit.test("triggers refresh", function() {
     client.trigger("refresh", 1);
 });
     
+QUnit.test("writeOscMessage functions writes correct data", function() {
+    var arrayBuffer = writeOscMessage("/tuio/2Dcur", [
+        {type: "s", value: "set"},
+        {type: "i", value: 1},
+        {type: "f", value: 5},
+        {type: "f", value: 6},
+        {type: "f", value: 7},
+        {type: "f", value: 8},
+        {type: "f", value: 9},
+    ]),
+        bufferView = new DataView(arrayBuffer);
+    
+    QUnit.equal( bufferView.getUint8(0), "/".charCodeAt());
+    QUnit.equal( bufferView.getUint8(1), "t".charCodeAt());
+    QUnit.equal( bufferView.getUint8(2), "u".charCodeAt());
+    QUnit.equal( bufferView.getUint8(3), "i".charCodeAt());
+    QUnit.equal( bufferView.getUint8(4), "o".charCodeAt());
+    QUnit.equal( bufferView.getUint8(5), "/".charCodeAt());
+    QUnit.equal( bufferView.getUint8(6), "2".charCodeAt());
+    QUnit.equal( bufferView.getUint8(7), "D".charCodeAt());
+    QUnit.equal( bufferView.getUint8(8), "c".charCodeAt());
+    QUnit.equal( bufferView.getUint8(9), "u".charCodeAt());
+    QUnit.equal( bufferView.getUint8(10), "r".charCodeAt());
+    QUnit.equal( bufferView.getUint8(11), 0);
+    QUnit.equal( bufferView.getUint8(12), ",".charCodeAt());
+    QUnit.equal( bufferView.getUint8(13), "s".charCodeAt());
+    QUnit.equal( bufferView.getUint8(14), "i".charCodeAt());
+    QUnit.equal( bufferView.getUint8(15), "f".charCodeAt());
+    QUnit.equal( bufferView.getUint8(16), "f".charCodeAt());
+    QUnit.equal( bufferView.getUint8(17), "f".charCodeAt());
+    QUnit.equal( bufferView.getUint8(18), "f".charCodeAt());
+    QUnit.equal( bufferView.getUint8(19), "f".charCodeAt());
+    QUnit.equal( bufferView.getUint8(20), 0);
+    QUnit.equal( bufferView.getUint8(21), 0);
+    QUnit.equal( bufferView.getUint8(22), 0);
+    QUnit.equal( bufferView.getUint8(23), 0);
+    QUnit.equal( bufferView.getUint8(24), "s".charCodeAt());
+    QUnit.equal( bufferView.getUint8(25), "e".charCodeAt());
+    QUnit.equal( bufferView.getUint8(26), "t".charCodeAt());
+    QUnit.equal( bufferView.getUint8(27), 0); 
+    QUnit.equal( bufferView.getUint32(28), 1);
+    QUnit.equal( bufferView.getFloat32(32), 5);
+    QUnit.equal( bufferView.getFloat32(36), 6);
+    QUnit.equal( bufferView.getFloat32(40), 7);
+    QUnit.equal( bufferView.getFloat32(44), 8);
+    QUnit.equal( bufferView.getFloat32(48), 9);
+});
+    
 QUnit.test("keeps track of Tuio1 cursors", function(assert) {
     
     var asyncDone = assert.async(),
-        arrayBuffer = new ArrayBuffer(1000),
-        ui8View = new Uint8Array(arrayBuffer),
-        bufferView = new DataView(arrayBuffer);
+        arrayBuffer;
     
     client.connect();
     
-    ui8View[0] = "/".charCodeAt();
-    ui8View[1] = "t".charCodeAt();
-    ui8View[2] = "u".charCodeAt();
-    ui8View[3] = "i".charCodeAt();
-    ui8View[4] = "o".charCodeAt();
-    ui8View[5] = "/".charCodeAt();
-    ui8View[6] = "2".charCodeAt();
-    ui8View[7] = "D".charCodeAt();
-    ui8View[8] = "c".charCodeAt();
-    ui8View[9] = "u".charCodeAt();
-    ui8View[10] = "r".charCodeAt();
-    ui8View[11] = 0;
-    // osc type tags start with comma
-    ui8View[12] = ",".charCodeAt();
-    // set string
-    ui8View[13] = "s".charCodeAt();
-    // s_id => integer
-    ui8View[14] = "i".charCodeAt();
-    // x_pos => float
-    ui8View[15] = "f".charCodeAt();
-    // y_pos
-    ui8View[16] = "f".charCodeAt();
-    // x_vel
-    ui8View[17] = "f".charCodeAt();
-    // y_vel
-    ui8View[18] = "f".charCodeAt();
-    // m_accel
-    ui8View[19] = "f".charCodeAt();
-    ui8View[20] = 0;
-    ui8View[21] = 0;
-    ui8View[22] = 0;
-    ui8View[23] = 0;
-    //actual values
-    // string set
-    ui8View[24] = "s".charCodeAt();
-    ui8View[25] = "e".charCodeAt();
-    ui8View[26] = "t".charCodeAt();
-    ui8View[27] = 0;
-    //s_id => 1
-    bufferView.setUint32(28, 1);
-    // x_pos is float
-    bufferView.setFloat32(32, 5);
-    // y_pos
-    bufferView.setFloat32(36, 6);
-    // x_vel
-    bufferView.setFloat32(40, 7);
-    // y_vel
-    bufferView.setFloat32(44, 8);
-    // m_accel
-    bufferView.setFloat32(48, 9);
+    arrayBuffer = writeOscMessage("/tuio/2Dcur", [
+        {type: "s", value: "set"},
+        {type: "i", value: 1},
+        {type: "f", value: 5},
+        {type: "f", value: 6},
+        {type: "f", value: 7},
+        {type: "f", value: 8},
+        {type: "f", value: 9},
+    ]);
     
     QUnit.equal( client.frameCursors.length, 0, "frameCursor length was not initially zero");
     // send
     setTimeout( function() {
         server.send(arrayBuffer);
-        server.close();
         QUnit.equal(client.frameCursors.length, 1, "Tuio.Client did not recognize a cursor message");
         QUnit.equal(client.frameCursors[0].sessionId, 1);
         QUnit.equal(client.frameCursors[0].xPos, 5);
@@ -112,74 +170,24 @@ QUnit.test("keeps track of Tuio1 cursors", function(assert) {
 QUnit.test("keeps track of Tuio2 pointers", function(assert) {
     
     var asyncDone = assert.async(),
-        arrayBuffer = new ArrayBuffer(1000),
-        ui8View = new Uint8Array(arrayBuffer),
-        bufferView = new DataView(arrayBuffer);
+        arrayBuffer;
     
     client.connect();
     
-    /**
-      * tuio2/ptr s_id tu_id c_id x_pos y_pos angle shear radius press
-      */
-    ui8View[0] = "/".charCodeAt();
-    ui8View[1] = "t".charCodeAt();
-    ui8View[2] = "u".charCodeAt();
-    ui8View[3] = "i".charCodeAt();
-    ui8View[4] = "o".charCodeAt();
-    ui8View[5] = "2".charCodeAt();
-    ui8View[6] = "/".charCodeAt();
-    ui8View[7] = "p".charCodeAt();
-    ui8View[8] = "t".charCodeAt();
-    ui8View[9] = "r".charCodeAt();
-    ui8View[10] = 0;
-    ui8View[11] = 0;
-    // osc type tags start with comma
-    ui8View[12] = ",".charCodeAt();
-    // s_id => integer
-    ui8View[13] = "i".charCodeAt();
-    // tu_id
-    ui8View[14] = "i".charCodeAt();
-    // c_id
-    ui8View[15] = "i".charCodeAt();
-    // all others floats
-    // x_pos
-    ui8View[16] = "f".charCodeAt();
-    // y_pos
-    ui8View[17] = "f".charCodeAt();
-    // angle
-    ui8View[18] = "f".charCodeAt();
-    // shear
-    ui8View[19] = "f".charCodeAt();
-    // radius
-    ui8View[20] = "f".charCodeAt();
-    // press
-    ui8View[21] = "f".charCodeAt();
-    ui8View[22] = 0;
-    ui8View[23] = 0;
-    //actual values
-    //s_id => 1
-    bufferView.setUint32(24, 1);
-    //tu_id, two 16-bit values
-    //t_id
-    ui8View[28] = 0;
-    ui8View[29] = 2;
-    //u_id
-    ui8View[30] = 0;
-    ui8View[31] = 3;
-    // c_id
-    bufferView.setUint32(32, 4);
-    // x_pos is float
-    bufferView.setFloat32(36, 5);
-    // y_pos
-    bufferView.setFloat32(40, 6);
-    // angle
-    bufferView.setFloat32(44, 7);
-    //shear
-    bufferView.setFloat32(48, 8);
-    //radius
-    bufferView.setFloat32(52, 9);
-    //press
-    bufferView.setFloat32(56, 10);
+    arrayBuffer = writeOscMessage("/tuio2/ptr", [
+        {type: "i", value: 1},
+        //tu_id, two 16-bit values
+        //t_id => 2, u_id => 3
+        // 0x00 0x02 0x00 0x03 => big endian 131075
+        {type: "i", value: 131075},
+        {type: "i", value: 4},
+        {type: "f", value: 5},
+        {type: "f", value: 6},
+        {type: "f", value: 7},
+        {type: "f", value: 8},
+        {type: "f", value: 9},
+        {type: "f", value: 10},
+    ]);
     
     QUnit.equal(client.frameCursors.length, 0, "frameCursor length was not initially zero");
     
