af132d43d02d4748142de0620fa1331b6f4724d7
Goran Košutić <kosutic.goran@gmail.com>
2015-11-24 18:29:33 +0100

    travis

--- test/unit/TuioClient.js
+++ test/unit/TuioClient.js
@@ -6,8 +6,6 @@ var client,
     server,
     osc = require("osc/dist/osc-browser");
     
-    console.log(osc);
-    
 function writeOscMessage(address, args) {
     
     var arrayBuffer = new ArrayBuffer(1000),
